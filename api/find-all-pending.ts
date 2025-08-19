ed6ed28a445e272c8a60ac0a0918273061cff132
Italo <italoteles399@gmail.com>
2025-08-19 07:16:58 -0300

    adicionado função handler de findAll

---
+++ api/find-all-pending.ts
@@ -0,0 +1,20 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import { connectDatabase } from "../src/config/mongo";
+import { EmailService } from "../src/services/EmailService";
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+    if (req.method !== "GET") {
+        return res.status(405).json({ message: "Método não permitido" });
+    }
+
+    try {
+        await connectDatabase();
+        const emailService = new EmailService();
+
+        const result = emailService.findAllEmails();
+        return res.json(result);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: " Erro na conexão com banco" });
+    }
+}
