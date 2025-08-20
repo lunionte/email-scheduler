a4cec74eaacca0509c311a0fc47ef418d90c7b38
italo <italoteles399@gmail.com>
2025-08-20 10:55:01 -0300

    correção no cors 2

--- api/create-email.ts
+++ api/create-email.ts
@@ -4,10 +4,10 @@ import { EmailService } from "../src/services/EmailService";
 import { connectDatabase } from "../src/config/mongo";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     if (req.method === "OPTIONS") {
-        res.setHeader("Access-Control-Allow-Origin", "*");
-        res.setHeader("Access-Control-Allow-Methods", "POST");
-        res.setHeader("Access-Control-Allow-Headers", "Content-Type");
         return res.status(200).end();
     }
     if (req.method !== "POST") {
